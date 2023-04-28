eda62ede06356b22ab61711b472ab117214da447
iscas-zac <yyzhangchi@126.com>
2023-04-28 15:12:48 +0800

    [fix/refactor]update the tune_html part, to utilize pest as parser

--- src/bin/tune_html.rs
+++ src/bin/tune_html.rs
@@ -39,51 +39,7 @@ fn handle_all(global_template: &str,
         .collect::<Vec<_>>()
         .join("\n");
 
-    let pairs = TagParser::parse(Rule::final_seg, &content)
-        .unwrap_or_else(|e| panic!("{}", e));
-        
-    let tags: Vec<_> = pairs.flat_map(|pair| {
-        pair.into_inner()
-            .filter(|ip| matches!(ip.as_rule(), Rule::single_tag))
-            .map(|pair| {
-                let mut main_tag = frictune::Tag { name: "''".into(), desc: None };
-                let mut trailers = vec![];
-                for inner in pair.into_inner() {
-                    match inner.as_rule() {
-                        Rule::leading_word => main_tag.name = format!("'{}'", inner.as_str().trim_matches('\"')),
-                        Rule::desc_leading_word => {
-                            let desc = inner.as_str().trim_matches('\"');
-                            if !desc.is_empty() {
-                                main_tag.desc = Some(format!("'{}'", desc));
-                            }
-                        },
-                        Rule::brace => {
-                            let mut trailing_tag = frictune::Tag { name: "''".into(), desc: None };
-                            let mut num = 1.0;
-                            for brace_inner in inner.into_inner() {
-                                match brace_inner.as_rule() {
-                                    Rule::inner_word => trailing_tag.name = format!("'{}'", brace_inner.as_str().trim_matches('\"')),
-                                    Rule::desc_inner_word => {
-                                        let desc = brace_inner.as_str().trim_matches('\"');
-                                        if !desc.is_empty() {
-                                            trailing_tag.desc = Some(format!("'{}'", desc));
-                                        }
-                                    },
-                                    Rule::number => num = brace_inner.as_str().parse::<f64>().unwrap(),
-                                    _ => { frictune::logger::naive::rupt(&format!("brace_inner is {}", brace_inner.as_str())); }
-                                }
-                            }
-                            if !trailing_tag.name.contains("''") {
-                                trailers.push((trailing_tag, num as f32));
-                            }
-                        },
-                        _ => { frictune::logger::naive::rupt(&format!("inner is {}", inner.as_str())); }
-                    }
-                }
-                update_database(db_conn, &main_tag, &trailers);
-                main_tag
-            })
-    }).collect();
+    let tags = extract_tags(&content, db_conn);
 
     for main_tag in tags.iter() {
         let name = main_tag.name.trim_matches('\'');
@@ -124,6 +80,55 @@ fn handle_all(global_template: &str,
     //println!("{}", reg.render("page", &env_json)?);
 
     Ok(())
+}
+
+fn extract_tags(content: &String, db_conn: &str) -> Vec<frictune::Tag> {
+    let pairs = TagParser::parse(Rule::final_seg, content)
+    .unwrap_or_else(|e| panic!("{}", e));
+        
+    let tags: Vec<_> = pairs.flat_map(|pair| {
+    pair.into_inner()
+        .filter(|ip| matches!(ip.as_rule(), Rule::single_tag))
+        .map(|pair| {
+            let mut main_tag = frictune::Tag { name: "''".into(), desc: None };
+            let mut trailers = vec![];
+            for inner in pair.into_inner() {
+                match inner.as_rule() {
+                    Rule::leading_word => main_tag.name = format!("'{}'", inner.as_str().trim_matches('\"')),
+                    Rule::desc_leading_word => {
+                        let desc = inner.as_str().trim_matches('\"');
+                        if !desc.is_empty() {
+                            main_tag.desc = Some(format!("'{}'", desc));
+                        }
+                    },
+                    Rule::brace => {
+                        let mut trailing_tag = frictune::Tag { name: "''".into(), desc: None };
+                        let mut num = 1.0;
+                        for brace_inner in inner.into_inner() {
+                            match brace_inner.as_rule() {
+                                Rule::inner_word => trailing_tag.name = format!("'{}'", brace_inner.as_str().trim_matches('\"')),
+                                Rule::desc_inner_word => {
+                                    let desc = brace_inner.as_str().trim_matches('\"');
+                                    if !desc.is_empty() {
+                                        trailing_tag.desc = Some(format!("'{}'", desc));
+                                    }
+                                },
+                                Rule::number => num = brace_inner.as_str().parse::<f64>().unwrap(),
+                                _ => { frictune::logger::naive::rupt(&format!("brace_inner is {}", brace_inner.as_str())); }
+                            }
+                        }
+                        if !trailing_tag.name.contains("''") {
+                            trailers.push((trailing_tag, num as f32));
+                        }
+                    },
+                    _ => { frictune::logger::naive::rupt(&format!("inner is {}", inner.as_str())); }
+                }
+            }
+            update_database(db_conn, &main_tag, &trailers);
+            main_tag
+        })
+        }).collect();
+    tags
 }
 
 fn construct_json_from_database(json: &mut serde_json::Value, tags: Vec<frictune::Tag>, db_conn: &str) {
