09235e22398b537fe89a49d3ae4210d69a8a2fbb
iscas-zac <yyzhangchi@126.com>
2023-05-10 22:40:14 +0800

    [feature/unfinished] prepare for wasm

--- src/db/gluesql.rs
+++ src/db/gluesql.rs
@@ -1,33 +1,33 @@
-
 use core::panic;
 use futures::executor::block_on;
+use gluesql::{sled_storage::SledStorage, prelude::{Glue, Payload, Row}, core::{executor::ValidateError, result}};
 
 pub struct Database {
-    conn: SqliteConnection,
+    conn: Glue<SledStorage>,
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum DatabaseError {
     UniqueViolation,
-    SqlxError(sqlx::Error),
+    GlueError(result::Error),
 }
 
 impl std::fmt::Display for DatabaseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::UniqueViolation => f.write_str("unique violation"),
-            Self::SqlxError(e) => f.write_str(e.to_string().as_str()),
+            Self::GlueError(e) => f.write_str(e.to_string().as_str()),
         }
     }
 }
 
 pub enum DatabaseResult {
     Success(String),
-    Things(Vec<sqlx::sqlite::SqliteRow>),
+    Things(Vec<Row>),
 }
 
-impl From<Vec<sqlx::sqlite::SqliteRow>> for DatabaseResult {
-    fn from(value: Vec<sqlx::sqlite::SqliteRow>) -> Self {
+impl From<Vec<Row>> for DatabaseResult {
+    fn from(value: Vec<Row>) -> Self {
         DatabaseResult::Things(value)
     }
 }
@@ -41,51 +41,46 @@ impl DatabaseResult {
         }
     }
 
-    pub fn get<'r, T>(&'r self, index: usize) -> Vec<T>
-    where
-        T: sqlx::Decode<'r, sqlx::sqlite::Sqlite> + sqlx::Type<sqlx::sqlite::Sqlite>,
+    pub fn get<T>(&self, index: usize) -> Vec<T>
     {
         match self {
             Self::Success(_) => vec![],
             Self::Things(v) =>
                 v.iter().map(|row|
-                    sqlx::Row::get::<T, usize>(row, index)
+                    row.get_value_by_index(index).unwrap().cast(T)
                 ).collect()
         }
     }
 }
 
-impl From<sqlx::sqlite::SqliteQueryResult> for DatabaseResult {
-    fn from(value: sqlx::sqlite::SqliteQueryResult) -> Self {
-        DatabaseResult::Success(format!("last inserted row is {}",
-            value.last_insert_rowid()))
+
+impl From<Vec<Payload>> for DatabaseResult {
+    fn from(values: Vec<Payload>) -> Self {
+        DatabaseResult::Things(values.into_iter().fold(vec![], |acc, payload|
+            match payload {
+                Payload::Select { labels, rows } => 
+                    [acc, rows].concat(),
+                _ => acc,
+            }
+        ))
     }
 }
 
-impl From<sqlx::Error> for DatabaseError {
-    fn from(value: sqlx::Error) -> Self {
+impl From<gluesql::core::result::Error> for DatabaseError {
+    fn from(value: gluesql::core::result::Error) -> Self {
         match value {
-            sqlx::Error::Database(dbe)
-                if dbe.is_unique_violation() => DatabaseError::UniqueViolation,
-            other => DatabaseError::SqlxError(other),
-
+            gluesql::core::result::Error::Validate(ValidateError::DuplicateEntryOnPrimaryKeyField(k))
+                => DatabaseError::UniqueViolation,
+            other => DatabaseError::GlueError(other),
         }
     }
 }
 
 impl Database {
     pub fn sync_new(db_url: &str) -> anyhow::Result<Database> {
-        block_on(async { Database::new(db_url).await } )
-    }
-
-    pub async fn new(db_url: &str) -> anyhow::Result<Database> {
-        if !sqlx::Sqlite::database_exists(&db_url).await? {
-            sqlx::Sqlite::create_database(&db_url).await?;
-        }
-
-        let mut conn = SqliteConnection::connect(&db_url).await?;
-
-        let query = sqlx::query("CREATE TABLE IF NOT EXISTS tags
+        let storage = SledStorage::new(db_url)?;
+        let mut conn = Glue::new(storage);
+        conn.execute("CREATE TABLE IF NOT EXISTS tags
         (
             tag_name    TEXT PRIMARY KEY NOT NULL,
             info     TEXT
@@ -99,48 +94,39 @@ impl Database {
             CONSTRAINT relationship_id1_fk FOREIGN KEY (tag1) REFERENCES tags(tag_name),
             CONSTRAINT relationship_id2_fk FOREIGN KEY (tag2) REFERENCES tags(tag_name),
             CONSTRAINT relation_pk PRIMARY KEY (tag1, tag2)
-        );");
-
-        match conn
-            .execute(query)
-            .await {
-            Ok(_) => Ok(Database { conn }),
-            Err(e) => { print!("err, {}", e); panic!() }
-        }
+        );")?;
+        Ok(Database { conn })
     }
-    
+
     pub async fn create(&mut self, table: &str, entry: &[String], data: &[String]) -> Result<DatabaseResult, DatabaseError> {
-        sqlx::query(
+        self.conn.execute_async(
             &format!("INSERT INTO {} ({}) VALUES({});", table, entry.join(", "), data.join(", "))
-        ).execute(&mut self.conn)
-            .await
+        ).await
             .map(|value| DatabaseResult::from(value))
             .map_err(|value| DatabaseError::from(value))
     }
 
     pub async fn delete(&mut self, table: &str, entry: &str, data: &str) -> Result<DatabaseResult, DatabaseError> {
-        sqlx::query(
+        self.conn.execute_async(
             &format!("DELETE FROM {}
                 WHERE {} = {}", table, entry, data)
-        ).execute(&mut self.conn)
-            .await
+        ).await
             .map(|value| DatabaseResult::from(value))
             .map_err(|value| DatabaseError::from(value))
     }
 
     pub async fn read(&mut self, table: &str, entry: &[String], cond: &str, opts: &str) -> Result<DatabaseResult, DatabaseError> {
-        sqlx::query(
+        self.conn.execute_async(
             &format!("SELECT {} FROM {}
                 WHERE {} {}", entry.join(", "), table, cond, opts)
-        ).fetch_all(&mut self.conn)
-            .await
+        ).await
             .map(|value| DatabaseResult::from(value))
             .map_err(|value| DatabaseError::from(value))
     }
     
     pub async fn update(&mut self, table: &str, entry: &[String], data: &[String],
             updated_entry: &[String], updated_data: &[String], cond: &str) -> Result<DatabaseResult, DatabaseError> {
-        sqlx::query(
+        self.conn.execute_async(
             &format!("INSERT INTO {} ({}) VALUES ({})
             ON CONFLICT DO
             UPDATE SET {}
@@ -152,9 +138,28 @@ impl Database {
                     format!("{} = {}", e, d)).collect::<Vec<_>>().join(", "),
                 cond
             )
-        ).execute(&mut self.conn)
-            .await
+        ).await
             .map(|value| DatabaseResult::from(value))
             .map_err(|value| DatabaseError::from(value))
+    }
+}
+
+fn main() {
+    let a = Database::sync_new("aaa");
+
+    let storage = SledStorage::new("data/doc-db").unwrap();
+    let mut glue = Glue::new(storage);
+
+    let sqls = vec![
+        "DROP TABLE IF EXISTS Glue;",
+        "CREATE TABLE Glue (id INTEGER);",
+        "INSERT INTO Glue VALUES (100);",
+        "INSERT INTO Glue VALUES (200);",
+        "SELECT * FROM Glue WHERE id > 100;",
+    ];
+
+    for sql in sqls {
+        let output = glue.execute(sql).unwrap();
+        println!("{:?}", output)
     }
 }
